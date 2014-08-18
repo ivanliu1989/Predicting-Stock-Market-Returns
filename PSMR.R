1b21c05d76815bf682d20b0ab6bf5bc40535434a
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-18 13:02:44 +1000

    function define

--- PSMR.R
+++ PSMR.R
@@ -34,11 +34,41 @@ library(xts)
 
 # newTA() can be used to create new
 # plotting functions for indicators that we wish to include in candlestick graphs.
-png("indicator.png")
-candleChart(last(GSPC, "3 months"), theme = "white", TA = NULL)
-avgPrice <- function(p) apply(HLC(p), 1, mean)
-addAvgPrice <- newTA(FUN = avgPrice, col = 1, legend = "AvgPrice")
-addT.ind <- newTA(FUN = T.ind, col = "red", legend = "tgtRet")
-addAvgPrice(on = 1)
-addT.ind()
-dev.off()
+    png("indicator.png")
+    candleChart(last(GSPC, "3 months"), theme = "white", TA = NULL)
+    avgPrice <- function(p) apply(HLC(p), 1, mean)
+    addAvgPrice <- newTA(FUN = avgPrice, col = 1, legend = "AvgPrice")
+    addT.ind <- newTA(FUN = T.ind, col = "red", legend = "tgtRet")
+    addAvgPrice(on = 1)
+    addT.ind()
+    dev.off()
+
+# a representative set of technical indicators, from those available in package TTR
+    library(TTR)
+    myATR <- function(x) ATR(HLC(x))[, "atr"]
+    mySMI <- function(x) SMI(HLC(x))[, "SMI"]
+    myADX <- function(x) ADX(HLC(x))[, "ADX"]
+    myAroon <- function(x) aroon(x[, c("High", "Low")])$oscillator
+    myBB <- function(x) BBands(HLC(x))[, "pctB"]
+    myChaikinVol <- function(x) Delt(chaikinVolatility(x[, c("High", "Low")]))[, 1]
+    myCLV <- function(x) EMA(CLV(HLC(x)))[, 1]
+    myEMV <- function(x) EMV(x[, c("High", "Low")], x[, "Volume"])[,2]
+    myMACD <- function(x) MACD(Cl(x))[, 2]
+    myMFI <- function(x) MFI(x[, c("High", "Low", "Close")], x[, "Volume"])
+    mySAR <- function(x) SAR(x[, c("High", "Close")])[, 1]
+    myVolat <- function(x) volatility(OHLC(x), calc = "garman")[,1]
+
+# Build a random forest using the data available for training:
+
+
+
+
+
+
+
+
+
+
+
+
+
