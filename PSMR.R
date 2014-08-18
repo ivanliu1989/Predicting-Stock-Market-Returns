c275d91fb847112eca458629f8c0d312321d1671
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-18 12:16:28 +1000

    function define

--- PSMR.R
+++ PSMR.R
@@ -2,14 +2,33 @@ setwd("C:\\Documents and Settings\\Macro\\Desktop\\Ivandata\\Predicting-Stock-Ma
 library(tseries)
 library(xts)
 
-ibm <- get.hist.quote("IBM",start="1970-01-02",end="2014-08-17", quote=c("Open", "High", "Low", "Close","Volume"))
-plot(ibm[,c('Close','Volume')],main="IBM stock")
+# As the function get.hist.quote() returns an object of class zoo, we have
+# again used the function as.xts() to coerce it to xts.
+    GSPC <- as.xts(get.hist.quote("^GSPC",start="1970-01-02",end="2009-09-15",
+                                  quote=c("Open", "High", "Low", "Close","Volume","AdjClose")))
+    GSPC[c(1,nrow(GSPC))]
 
-ts(rnorm(25), frequency = 4, start = c(1959, 2))
-ibm[c(1,nrow(ibm)),]
+# quantmod
+    library(quantmod)
+    getSymbols("^GSPC", from = "1970-01-01", to = "2009-09-15")
+    colnames(GSPC) <- c("Open", "High", "Low", "Close", "Volume", "AdjClose")
+    
+    setSymbolLookup(IBM=list(name='IBM',src='yahoo'),
+                    USDEUR=list(name='USD/EUR',src='oanda'))
+    getSymbols(c('IBM','USDEUR'))
+    head(IBM)
+    head(USDEUR)
+    # saveSymbolLookup()
+    # loadSymbolLookup()
 
-h.returns <- function(x,h=1) {
-    diff(x,lag=h)/x[1:(length(x)-h)]
-}
-h.returns(c(45,23,4,56,-45,3),h=2)
+# indicator function
+    T.ind <- function(quotes, tgt.margin = 0.025, n.days = 10) {
+        v <- apply(HLC(quotes), 1, mean)
+        r <- matrix(NA, ncol = n.days, nrow = NROW(quotes))
+        for (x in 1:n.days) r[, x] <- Next(Delt(v, k = x), x)
+        x <- apply(r, 1, function(x) sum(x[x > tgt.margin | x < -tgt.margin]))
+        if (is.xts(quotes))
+            xts(x, time(quotes))
+        else x
+    }
 
