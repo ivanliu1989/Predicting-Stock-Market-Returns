bb6eb18b321f4a845241c183f0d963b3d3151037
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-18 10:18:01 +1000

    data exploration

--- PSMR.R
+++ PSMR.R
@@ -1,3 +1,4 @@
 setwd("C:\\Documents and Settings\\Macro\\Desktop\\Ivandata\\Predicting-Stock-Market-Returns")
 library(tseries)
 t.ibm <- get.hist.quote("IBM",start="1970-01-02",end="2002-05-17", quote=c("Open", "High", "Low", "Close","Volume"))
+plot(t.ibm[,c('Close','Volume')],main="IBM stock")
