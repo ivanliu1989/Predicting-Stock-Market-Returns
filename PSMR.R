a85c241d2ee7b632f7f785df84d159374972b847
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-18 10:56:31 +1000

    data exploration

--- PSMR.R
+++ PSMR.R
@@ -5,3 +5,10 @@ plot(ibm[,c('Close','Volume')],main="IBM stock")
 
 ts(rnorm(25), frequency = 4, start = c(1959, 2))
 ibm[c(1,nrow(ibm)),]
+
+h.returns <- function(x,h=1) {
+    diff(x,lag=h)/x[1:(length(x)-h)]
+}
+h.returns(c(45,23,4,56,-45,3),h=2)
+
+library(ts)
