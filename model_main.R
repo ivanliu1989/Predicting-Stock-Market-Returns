1be1ae8a82c285be2dea9a89a58482019524e8f6
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-19 16:45:16 +1000

    Testing

--- model_main.R
+++ model_main.R
@@ -1,13 +1,14 @@
 setwd("C:\\Documents and Settings\\Macro\\Desktop\\Ivandata\\Predicting-Stock-Market-Returns\\final model")
 
 # getting the quotes for the testing period
-user.quote <- "IBM"
+user.quote <- "TAP.AX"
 library(xts)
 library(tseries)
 library(quantmod)
 quote.data <- as.xts(get.hist.quote(user.quote,start="1970-01-02",quote=c("Open", "High", "Low", "Close","Volume","AdjClose")))
 colnames(quote.data) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted")
 quote.data[c(1,nrow(quote.data))]
+nrow(quote.data)
 
 # Train and test periods
 start <- 1
@@ -52,30 +53,36 @@ data.model <- specifyModel(T.ind(quote.data) ~ Delt(Cl(quote.data),k=1:10) +
                                mySAR(quote.data) + runMean(Cl(quote.data)) + runSD(Cl(quote.data)))
 
 # transfer training and testing stock data into data.frame
-Tdata.train <- as.data.frame(modelData(data.model, data.window=c(start,len.tr)))
-Tdata.eval <- na.omit(as.data.frame(modelData(data.model, data.window=c((start+len.tr),(start+len.tr+len.ts-1)))))
+Tdata.train <- as.data.frame(modelData(data.model, data.window=c("1996-09-30","2011-07-05")))
+Tdata.eval <- na.omit(as.data.frame(modelData(data.model, data.window=c("2011-07-06","2014-08-18"))))
 Tform <- as.formula('T.ind.quote.data ~ .')
 
 # Subsetting the train data
 date <- rownames(Tdata.train[nrow(Tdata.train),])
-market <- quote.data[paste(date,'/',sep='')][1:len.ts]
+market <- quote.data[paste(date,'/',sep='')][2:len.ts]
 
 # learning the model and obtaining its signal predictions
-library(e1071)
+set.seed(1234)
+library(nnet)
 library(DMwR)
-s <- svm(Tform,Tdata.train[tr,],cost=10,gamma=0.01)
-p <- predict(s,Tdata.train[ts,])
-sig <- trading.signals(p,0.1,-0.1)
+norm.data <- scale(Tdata.train)
+nn <- nnet(Tform, norm.data[1:1000, ], size = 10, decay = 0.01,maxit = 1000, linout = T, trace = F)
+norm.preds <- predict(nn, norm.data[1001:nrow(norm.data), ])
+preds <- unscale(norm.preds, norm.data)
+sigs.nn <- trading.signals(preds, 0.1, -0.1)
+true.sigs <- trading.signals(Tdata.train[1001:nrow(Tdata.train), "T.ind.quote.data"],0.1, -0.1)
+sigs.PR(sigs.nn, true.sigs) 
+
 # now using the simulated trader
 source("Trading policy.R")
-t1 <- trading.simulator(market,sig,'policy.1',list(exp.prof=0.05,bet=0.2,hold.time=30))
+t1 <- trading.simulator(market,true.sigs,'policy.1',list(exp.prof=0.05,bet=0.2,hold.time=30))
 t1
 summary(t1)
 tradingEvaluation(t1)
 plot(t1, market, theme = "white", name = "SP500")
 
 source("Trading policy2.R")
-t2 <- trading.simulator(market, sig, "policy.2", list(exp.prof = 0.05, bet = 0.3))
+t2 <- trading.simulator(market, true.sigs, "policy.2", list(exp.prof = 0.05, bet = 0.3))
 summary(t2)
 tradingEvaluation(t2)
 plot(t2, market, theme = "white", name = "SP500")
