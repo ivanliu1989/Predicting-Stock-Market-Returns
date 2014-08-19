710451752787c75979a079b8e7727cf6ea1e101d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-19 16:57:06 +1000

    Testing

--- model_main.R
+++ model_main.R
@@ -65,6 +65,9 @@ market <- quote.data[paste(date,'/',sep='')][2:len.ts]
 set.seed(1234)
 library(nnet)
 library(DMwR)
+source("Monte Carlo.R")
+grow
+
 norm.data <- scale(Tdata.train)
 nn <- nnet(Tform, norm.data[1:1000, ], size = 10, decay = 0.01,maxit = 1000, linout = T, trace = F)
 norm.preds <- predict(nn, norm.data[1001:nrow(norm.data), ])
